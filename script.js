8f24b00c6acd3697403c819b215efa6ea1ebe4b1
rashed <125754285+Rashed-ux920@users.noreply.github.com>
2025-04-21 17:51:25 -0500

    css add

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ AOS.init({
 
 // tying effect 
 
-const typed = new Typed('#typing',{
-    string:["UI/UX Designer.", "Web Developer.","Creative Thinker."]
+const typed = new Typed("#typing",{
+    strings:["UI/UX Designer.", "Web Developer.","Creative Thinker."],
+    typeSpeed:60,
+    backSpeed:30,
+    backDelay:1000,
+    loop:true,
 })
