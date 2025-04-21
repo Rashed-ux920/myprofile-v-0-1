1b683af2a47ca6877e2ca1ea82260ecdf0fbfbc4
rashed <125754285+Rashed-ux920@users.noreply.github.com>
2025-04-21 16:31:03 -0500

    first commit

---
+++ read.md
@@ -0,0 +1,9 @@
+## Color Palette for #3498db
+
+```json
+{
+  "color50": "#eaf6fd",
+  "color100": "#d5ecfb",
+  
+  "color900": "#0a2b4f"
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+AOS.init({
+    duration: 1000,
+    once: true
+})
+
+// tying effect 
+
+const typed = new Typed('#typing',{
+    string:["UI/UX Designer.", "Web Developer.","Creative Thinker."]
+})
